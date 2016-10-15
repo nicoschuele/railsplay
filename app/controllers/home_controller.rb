87e80d519d34b633f213e8324d0a01f67fe31181
Nico Schuele <nicoschuele@gmail.com>
2016-10-15 20:01:21 +0200

    Show number of records on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @companies_count = Company.count
+    @categories_count = Category.count
+    @products_count = Product.count
   end
 end

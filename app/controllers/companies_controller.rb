abba9abfb193b45a11c8921ef96ddf8cbec65ae9
Nico Schuele <nicoschuele@gmail.com>
2016-10-15 20:37:39 +0200

    Implement CRUD for companies

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,60 @@
+class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @companies = Company.all
+  end
+
+  def show
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company, notice: "#{@company.name} was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @company.update(company_params)
+      redirect_to @company, notice: "#{@company.name} was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @company.destroy
+    redirect_to companies_url, notice: "#{@company.name} was successfully destroyed."
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:id])
+  rescue
+    redirect_to root_url
+  end
+
+  def company_params
+    params.require(:company).permit(
+      :name,
+      :slogan,
+      :mission_statement,
+      :street,
+      :zip_code,
+      :city,
+      :country,
+      :revenue
+    )
+  end
+end

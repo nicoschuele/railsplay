06e13875176a5b4912b316a48aaa6c1286834ee1
Nico Schuele <nicoschuele@gmail.com>
2016-10-16 13:10:08 +0200

    Order products by name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all
+    @products = Product.order('LOWER(name)')
   end
 
   def show
